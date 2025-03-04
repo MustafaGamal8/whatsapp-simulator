d66912948e6e02ff98bd289a0e46c7092157f3f7
Mustafa Gamal <mustafa.gamal.elsayed@gmail.com>
2025-03-04 08:10:48 +0200

    FIX: Client Import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
-import { ClientsModule } from './clients/clients.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [WhatsappModule, ClientsModule, UsersModule],
+  imports: [WhatsappModule,  UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
